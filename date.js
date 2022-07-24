25902e454644c0e0f1451f7f784e169e92ccc137
Attila Szep <108997619+szep-attila@users.noreply.github.com>
2022-07-24 08:50:54 +0100

    Create date.js

---
+++ date.js
@@ -0,0 +1,12 @@
+var mydate=new Date()
+var year=mydate.getYear()
+if (year < 1000)
+year+=1900
+var day=mydate.getDay()
+var month=mydate.getMonth()+1
+if (month<10)
+month="0"+month
+var daym=mydate.getDate()
+if (daym<10)
+daym="0"+daym
+document.write("<small><font color='000000' face='Arial'><b>"+year+"/"+month+"/"+daym+"</b></font></small>")
